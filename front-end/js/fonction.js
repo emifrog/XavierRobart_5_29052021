7b726c662cf47472d672b8f52d885223a8674698
emifrog <emifrog@hotmail.fr>
2021-06-28 00:56:32 +0200

    recuperation des donnees de l api

--- front-end/js/fonction.js
+++
@@ -1,4 +0,0 @@
-const url = "http://localhost:3000/api/teddies";
-let products = [];
-
-// Produits //
--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,35 +1,30 @@
-let teddies;
+function getRequest(){
 
-// récupérer les données de l'API avec La promesse Fetch
-const fetchTeddies = async("http://localhost:3000/api/teddies"); {
-    teddies = await fetch("http://localhost:3000/api/teddies" )
-    .then(response => { return response.json(); })
-    .catch((error) => {
-        console.log('Probléme avec fetch:' + error.message);
-    });    
-};
+const fetchPromise = fetch("http://localhost:3000/api/teddies");
+const inputJS = document.getElementById("produits");
 
-// intégrer les information dans le HTML et inclure le lien pour récupéré l'id du produit dans l'Url qui correspondra bien a la page du produit
-const showTeddies = async(results) => {
-    await fetchTeddies(url);
-    if (results) {
-        results.innerHTML = (
-            teddies
-            .map(teddie => (
-                `<div class="card" style="width:18rem;">
-                    <img class="card-img-top teddies-img" src="${teddie.imageUrl}" alt ="${teddie.name}" />
-                    <div class="card-body">
-                        <h2 class="card-title name">${teddie.name}</h2>
-                        <p class="card-text price">${teddie.price / 100} €</p> 
-                        <a href="produit.html?id=${teddie._id}" class="btn btn-primary">Détails du produit</a>
-                    </div>
-                </div>
-                `
-            ).join('')
-        ));
-    }
-};
+fetchPromise.then(response => {        //on exécute la promesse
+  return response.json();              // on récupère le résultat sous format json
+})
+.then((data => {
+  data.forEach((item)  => {            //pour chaque item récupéré de l'API, on crée une constante name, price_id etc..
+    const { name, price, _id, description, imageUrl } = item;
+      //puis on affiche ces informations sous forme HTML
+         inputJS.innerHTML +=`
+            <div class="container col-md-6 col-lg-4">
+               <div class="affichage_produit mt-4 card bg-white">
+                  <img class=”card-img-top” src="${imageUrl}"  alt="appareil ${name}">
+                  <div class="card-body">
+                     <h3 class="card-title">${name}</h3>
+                     <span>${price/100}€</span></p>
+                     <p class="card-text product-description">${description}</p>
+                     <div class="text-center mt-4" ><a id="bouton" type="button" class="btn btn-secondary text-white" onclick="window.location.href = 'produit.html?id=${_id}'">En savoir plus</a></div>
+                  </div>
+               </div>
+            </div>`
+   })
+}))
 
-showTeddies(document.getElementById("results"));
-//function qui affichae dans le span la quantité d'article
-showQuantity(localStorage.getItem('product'));
+}
+getRequest()
+cartNumber()
